28dfebe8653ccd4d367239bcdf112b1b1b9f93b2
Erdene-Ochir Tuguldur <tugstugi@gmail.com>
2013-12-11 20:29:59 +0100

    login form added

--- app/init.js
+++ app/init.js
@@ -3,8 +3,8 @@ function onStart () {
 	// TODO : Add your Initilize code here
 	// NOTE : In order to start your app, call "sf.start()" at the end of this function!!
 	
-	sf.scene.show('Loading');
-	sf.scene.focus('Loading');
+	sf.scene.show('Login');
+	sf.scene.focus('Login');
 }
 function onDestroy () {
 	//stop your XHR or Ajax operation and put codes to distroy your application here
---
+++ app/scenes/Login.js
@@ -0,0 +1,105 @@
+alert('SceneLogin.js loaded');
+
+function SceneLogin() {
+
+};
+
+SceneLogin.prototype.initialize = function () {
+	alert("SceneLogin.initialize()");
+	// this function will be called only once when the scene manager show this scene first time
+	// initialize the scene controls and styles, and initialize your variables here
+	// scene HTML and CSS will be loaded before this function is called
+	$('#usernameTextInput').sfTextInput({
+		text:'',
+		maxlength:20,
+		ontextchanged: function (text) {
+			
+		},
+		oncomplete: function (text) {
+			
+		},
+		onkeypadchanged: function (keypadtype) {
+			var width = $('#usernameTextInput').width();
+			var offset = $('#usernameTextInput').offset();
+			if (keypadtype.toLowerCase() === '12key') {
+				
+			} else {
+				
+			}
+			$('#usernameTextInput').sfTextInput('setKeypadPos', offset.left + width + 10, offset.top);
+		}
+	});
+	$('#passwordTextInput').sfTextInput({
+		text:'',
+		maxlength:20,
+		ontextchanged: function (text) {
+			
+		},
+		oncomplete: function (text) {
+			
+		},
+		onkeypadchanged: function (keypadtype) {
+			var width = $('#passwordTextInput').width();
+			var offset = $('#passwordTextInput').offset();
+			if (keypadtype.toLowerCase() === '12key') {
+				
+			} else {
+				
+			}
+			$('#passwordTextInput').sfTextInput('setKeypadPos', offset.left + width + 10, offset.top);
+		}
+	});
+	$('#loginButton').sfButton({
+		text:'Login'
+	});
+	$('#usernameLabel').sfLabel({
+		text:'Username:'
+	});
+	$('#passwordLabel').sfLabel({
+		text:'Password:'
+	});
+};
+
+SceneLogin.prototype.handleShow = function (data) {
+	alert("SceneLogin.handleShow()");
+	// this function will be called when the scene manager show this scene
+};
+
+SceneLogin.prototype.handleHide = function () {
+	alert("SceneLogin.handleHide()");
+	// this function will be called when the scene manager hide this scene
+};
+
+SceneLogin.prototype.handleFocus = function () {
+	alert("SceneLogin.handleFocus()");
+	// this function will be called when the scene manager focus this scene
+};
+
+SceneLogin.prototype.handleBlur = function () {
+	alert("SceneLogin.handleBlur()");
+	// this function will be called when the scene manager move focus to another scene from this scene
+};
+
+SceneLogin.prototype.handleKeyDown = function (keyCode) {
+	alert("SceneLogin.handleKeyDown(" + keyCode + ")");
+	// TODO : write an key event handler when this scene get focued
+	switch (keyCode) {
+		case sf.key.LEFT:
+			break;
+		case sf.key.RIGHT:
+			
+			break;
+		case sf.key.UP:
+			
+			break;
+		case sf.key.DOWN:
+			$('#passwordTextInput').sfTextInput('focus');
+			break;
+		case sf.key.ENTER:
+			
+			break;
+		default:
+			alert("handle default key event, key code(" + keyCode + ")");
+			break;
+	}
+};
